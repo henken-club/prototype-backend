fe36134b48cbca9fa05e05682802cb482c69e852
SnO2WMaN <me@sno2wman.net>
2021-08-11 16:50:30 +0900

    feat: searchAuthors

--- src/authors/authors.resolver.ts
+++ src/authors/authors.resolver.ts
@@ -11,6 +11,7 @@ import {InternalServerErrorException, UseGuards} from '@nestjs/common';
 import {AuthorsService} from './authors.service';
 import {AuthorArray, AuthorEntity} from './authors.entities';
 import {AddAuthorArgs, AddAuthorPayload} from './dto/add-author.dto';
+import {SearchAuthorsArgs} from './dto/search-authors.dto';
 
 import {GraphQLJwtGuard} from '~/auth/graphql-jwt.guard';
 import {Viewer, ViewerType} from '~/auth/viewer.decorator';
@@ -54,6 +55,22 @@ export class AuthorsResolver {
   async getAllBooks(): Promise<AuthorArray> {
     const nodes = await this.authorsService.getAll();
     return {nodes};
+  }
+
+  @Query(() => AuthorArray, {name: 'searchAuthors'})
+  async searchAuthors(
+    @Args() {query, skip, limit}: SearchAuthorsArgs,
+  ): Promise<AuthorArray> {
+    try {
+      const nodes = await this.authorsService.searchAuthors(query, {
+        skip,
+        limit,
+      });
+      // const count = await this.authorsService.countSearchAuthors(query);
+      return {nodes};
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
   }
 
   @Mutation(() => AddAuthorPayload, {name: 'addAuthor'})
